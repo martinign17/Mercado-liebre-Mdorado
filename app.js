523a8075044848046e06251941141153b354a50d
martinign17 <martinign94@gmail.com>
2023-07-27 02:37:53 -0300

    a

--- app.js
+++ app.js
@@ -2,24 +2,25 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-
-
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.resolve(__dirname, '/public')));
+// Corregir la ruta del directorio estático
+app.use(express.static(path.resolve(__dirname, 'public')));
 
-app.listen(PORT, () =>{
-    console.log('Aplicacion escuchando en puerto ${PORT}');
+app.listen(PORT, () => {
+  console.log(`Aplicacion escuchando en puerto ${PORT}`);
 });
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/views/home.html')
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
 
-app.get('/register', (req,res) =>{
-    res.sendFile(__dirname = '/views/register.html')
-})
+// Corregir el operador de asignación en la ruta /register
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'register.html'));
+});
 
-app.get('/login', (req,res) =>{
-    res.sendFile(__dirname = '/views/login.html')
-})
+// Corregir el operador de asignación en la ruta /login
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
