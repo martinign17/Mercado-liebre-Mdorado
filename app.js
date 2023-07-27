1bd363d98ff9def52b0421c4e4b896b520695b77
martinign17 <martinign94@gmail.com>
2023-07-27 03:05:17 -0300

    Animacion del camion

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
-// Corregir la ruta del directorio estático
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.listen(PORT, () => {
@@ -15,12 +14,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
 
-// Corregir el operador de asignación en la ruta /register
 app.get('/register', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'register.html'));
 });
 
-// Corregir el operador de asignación en la ruta /login
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
